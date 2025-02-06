e37c97ac7d5dba688e6f73f647d3f745c16844fb
Strangerdanger2 <matissgraud1892@gmail.com>
2025-02-06 16:38:06 +0200

    Pievienoju 5 uzd

--- 05_v2.sql
+++ 05_v2.sql
@@ -3,11 +3,9 @@
 -- Parādiet visus skolēnus un skolotājus, kuri ir piešķīruši atzīmes. Norādiet skolēna vārdu, uzvārdu
 -- un skolotāja vārdu, uzvārdu.
 
-SELECT skoleni.vards, skoleni.uzvards, atzimes.atzime, skolotaji.vards, skolotaji.uzvards
-FROM atzimes
+SELECT skolotaji.id, skoleni.vards, skoleni.uzvards, atzimes.atzime, skolotaji.vards, skolotaji.uzvards
+FROM skoleni
 
-JOIN skoleni ON atzimes.skolena_id = skoleni.id
+JOIN atzimes ON atzimes.skolena_id = skoleni.id
 JOIN skolotaji ON atzimes.skolotaja_id = skolotaji.id
-
-GROUP BY 
 ;
